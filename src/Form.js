deaef34ca30bb2c6ceec6c3c428876b80d510954
charlescortez <charlesmph@gmail.com>
2019-09-06 15:29:31 -0400

    added search and checkbox

---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Form() {
+    return(
+    <div>
+        <form>
+            <input type='text' placeholder="Search..."/>
+            <input type="checkbox" />
+            <span>Only show items in stock</span>
+        </form>
+    </div>
+        
+    )
+}
+
+export default Form;
